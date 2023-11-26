245bb72af72bd9a4ac2a62311eeba8fe0de1a6bf
sparsh985 <90522171+sparsh985@users.noreply.github.com>
2023-11-26 12:35:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 1st!";
+  question.innerHTML = "Yay, Ash! see you on the 1st!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
